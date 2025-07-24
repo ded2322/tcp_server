17f7d5fcd097074cf49f412ba8a827151d417080
ded <otheremails602@gmail.com>
2025-07-24 21:12:04 +0300

    #TCP corrected exeptions

--- utils_tcp.cpp
+++ utils_tcp.cpp
@@ -22,21 +22,22 @@ void UserConnection::closeUserConnection() {
 std::optional<std::string> UserConnection::readMessage() {
     // TODO create loop for reading long message
     // TODO  corrent EINTR and EAGAIN error
-    if(client_sock < 0) throw strerror(errno);
+    if ( client_sock < 0 ) throw "Failed read message: no client";
 
     char buffer[1024] = {0};
-    int bytes = read(client_sock, buffer, 1024);
+    ssize_t bytes = read(client_sock, buffer, 1024);
 
-    if ( bytes <= 0 ) return std::nullopt;
+    if ( bytes == 0 ) return std::nullopt;
+    if ( bytes < 0 ) throw sprintf((char*)"read: %s (%d)", strerror(errno), errno);
 
     return std::string(buffer);
 } 
 
 void UserConnection::sendMessage(const std::string&& message) {
     // TODO in future do loop, which can accept long message
-    if (client_sock < 0) throw "Failed send message: no client";
+    if ( client_sock < 0 ) throw "Failed send message: no client";
 
     ssize_t byte_send = send(client_sock, message.c_str(), message.length(), MSG_NOSIGNAL);
 
-    if (byte_send < 0) throw "Failed send message\n";
+    if (byte_send < 0) throw sprintf((char*)"send: %s (%d)", strerror(errno), errno);
 }
