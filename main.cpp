1f055faa6e509501dd2abe82cdfc0093adc7f756
ded <otheremails602@gmail.com>
2025-08-07 01:01:25 +0300

    #TCP fixed threading

--- main.cpp
+++ main.cpp
@@ -3,28 +3,13 @@
 *** Main cpp file, where start server 
 ****************************
 */
-#define MAYBE_UNUSED __attribute__((unused))
-#define COUNT_THREADS 5
-
 #include <iostream>
-#include <cstdio>
-#include <string>
-#include <thread>
-#include <atomic>
 #include <csignal>
 #include <cstring>
-#include <memory>
-#include <vector>
 
-#include "tcp_server.h"
-// TODO write to file for client
-// TODO include flags
-
-std::atomic<bool> server_is_running {true};
+#include "thead_pull.h"
 
 static struct sigaction sa;
-
-// Cmake ругается что не определено заранее
 static void terminate([[maybe_unused]] int signum) {
     server_is_running = false;
 }
@@ -38,6 +23,7 @@ int main() {
     sigaction(SIGINT, &sa, 0);
 
     TcpServer server(8080);
+    
     try {
         server.startTcpServer();
     } catch (const char* exep) {
@@ -45,32 +31,40 @@ int main() {
         return -1;
     }
 
-    std::vector<std::thread> threads(COUNT_THREADS);
-    std::vector<int> sock_client(COUNT_THREADS);
+    ThreadPool thread_pool {server};
+    try {
+        thread_pool.start_worker(COUNT_THREADS);
+    } catch (const char* exep) {
+        std::cerr << "thread_pool: " << exep << "\n";
+        thread_pool.stop_worker();
+        return -1;
+    }
+    // std::vector<std::thread> threads(COUNT_THREADS);
+    // std::vector<int> sock_client(COUNT_THREADS);
 
 
-    for (std::size_t thread {0}; thread < COUNT_THREADS; ++thread) {
-        while (server_is_running) {
-            try{
+    // for (std::size_t thread {0}; thread < COUNT_THREADS; ++thread) {
+    //     while (server_is_running) {
+    //         try{
 
-               std::unique_ptr<Connection> user_connection = server.acceptClient();
+    //            std::unique_ptr<Connection> user_connection = server.acceptClient();
                
-                threads.emplace_back( [&server] (std::unique_ptr<Connection> connection) mutable {
-                    server.handlerClient(connection);
-                    std::cout << "Connection with client close\n";
-                }, std::move(user_connection));
+    //             threads.emplace_back( [&server] (std::unique_ptr<Connection> connection) mutable {
+    //                 server.handlerClient(connection);
+    //                 std::cout << "Connection with client close\n";
+    //             }, std::move(user_connection));
                 
-            } catch (const char* exep) {
-                std::cerr << "User_connection: " << exep << "\n";
-            }
-        }
-    }
+    //         } catch (const char* exep) {
+    //             std::cerr << "User_connection: " << exep << "\n";
+    //         }
+    //     }
+    // }
     
-    for (auto& thread: threads) {
-        if (thread.joinable()) {
-            thread.join();
-        }
-    }
+    // for (auto& thread: threads) {
+    //     if (thread.joinable()) {
+    //         thread.join();
+    //     }
+    // }
 
     server.~TcpServer();
     std::cout << "Server turn off\n";
